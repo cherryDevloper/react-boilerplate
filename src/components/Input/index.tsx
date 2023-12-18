b646c87655a807a2af2a69bc65105b28225d97dc
sharare <sharareshaddev@gmail.com>
2023-12-18 10:56:57 +0330

    added input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, FC } from 'react';
+
+interface InputProps {
+  type: 'text' | 'number' | 'email' | 'password';
+  label: string;
+  value: string | number;
+  name: string;
+  placeholder: string;
+  error: boolean;
+  disabled?: boolean;
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: FC<InputProps> = ({
+  type,
+  label,
+  value,
+  name,
+  placeholder,
+  error,
+  disabled,
+  onChange,
+}) => {
+  return (
+    <div className="input-wrapper">
+      <label htmlFor={label}>{label}</label>
+      <input
+        type={type}
+        id={label}
+        value={value}
+        name={name}
+        placeholder={placeholder}
+        onChange={onChange}
+        disabled={disabled}
+      />
+      {error && <p className="error">Input filed can't be empty!</p>}
+    </div>
+  );
+};
+
+export default Input;
